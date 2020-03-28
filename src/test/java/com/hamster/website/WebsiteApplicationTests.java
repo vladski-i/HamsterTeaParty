909673377f1258b47f1ea0af2a2b5b7553ab850c
thomas.palade <thomas.e.palade@gmail.com>
2020-03-28 21:04:57 +0200

    deleted test units (ca sa mearga la toti)

--- src/test/java/com/hamster/website/WebsiteApplicationTests.java
+++ src/test/java/com/hamster/website/WebsiteApplicationTests.java
@@ -1,13 +1,8 @@
 package com.hamster.website;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WebsiteApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
